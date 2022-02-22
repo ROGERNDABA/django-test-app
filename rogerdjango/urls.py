7d3ba390a78c489de889a5123925606155c4b433
Rogerndaba <rogerndaba@gmail.com>
2022-02-22 12:55:20 +0200

    happy to say the basic of tailwind is working

--- rogerdjango/urls.py
+++ rogerdjango/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('babysteps/', include('babysteps.urls')),
     path('admin/', admin.site.urls),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
--- rogerdjangotailwind/static_src/tailwind.config.js
+++ rogerdjangotailwind/static_src/tailwind.config.js
@@ -12,7 +12,8 @@ module.exports = {
      * Set mode to "jit" if you want to generate your styles on-demand as you author your templates;
      * Set mode to "aot" if you want to generate the stylesheet in advance and purge later (aka legacy mode).
      */
-    mode: "jit",
+    mode: "aot",
+    darkMode: 'class',
 
     purge: [
         /**
@@ -22,13 +23,13 @@ module.exports = {
         /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
         '../templates/**/*.html',
 
-        /* 
+        /*
          * Main templates directory of the project (BASE_DIR/templates).
          * Adjust the following line to match your project structure.
          */
         '../../templates/**/*.html',
-        
-        /* 
+
+        /*
          * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
          * Adjust the following line to match your project structure.
          */
@@ -49,7 +50,6 @@ module.exports = {
          */
         // '../../**/*.py'
     ],
-    darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
     },
